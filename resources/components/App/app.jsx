df9db68ad1803bc332b0bd46e313ee9c738d30ad
laelfab <laelfab@gmail.com>
2021-02-02 10:15:48 +0000

    installation de react et configuration

---
+++ resources/components/App/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const App = () =>  (
+    <h1>
+        laravel and react
+    </h1>
+)
+
+
+
+
+
+
+export default App;
+
+
---
+++ resources/components/index.jsx
@@ -0,0 +1,10 @@
+import {render} from 'react-dom'
+import React from 'react'
+import App from './App/app.jsx'
+
+render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,   
+    document.getElementById('root')
+);
